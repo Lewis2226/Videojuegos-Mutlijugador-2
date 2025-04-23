42c1caa89609ffaee0a6ca64bc24a629a1b95093
L <ivanrobleslazaro@gmail.com>
2025-04-23 10:59:41 -0600

    avance de 24/04/25
    Tengo que poner la animación de caminar y idle

--- Assets/Proyecto/Scripts/PlayerController.cs
+++ Assets/Proyecto/Scripts/PlayerController.cs
@@ -7,10 +7,31 @@ public class PlayerController : NetworkBehaviour
     Vector3 desiredDirection;
     public float speed = 1.0f;
 
+    //salud del jugador
+    //public int health = 100;
+
+    //networkvariable para replicar la vida
+    NetworkVariable<int> health = new NetworkVariable<int> (100,NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    private UiManager hud;
+
+    [Header("SFX")]
+    public AudioClip DamageSound;
+    public AudioClip DeathSound;
+    AudioSource audioSource;
+
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("Hola mundo soy un " + (IsClient? "cliente" : "servidor"));
+        Debug.Log("IsClient = " + IsClient + ", IsServer = " + IsServer + ", IsHost = " + IsHost);
+        Debug.Log(name + " Is owner " + IsOwner);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        hud = GameObject.Find("GameManager").GetComponent<UiManager>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -19,15 +40,80 @@ public class PlayerController : NetworkBehaviour
         if (IsOwner)
         {
             desiredDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            desiredDirection.Normalize();
 
-            float mag = desiredDirection.magnitude;
-
-            if(mag > 0)
+            if (IsAlive())
             {
-                transform.forward = desiredDirection;
-                transform.Translate(0, 0, speed * Time.deltaTime);
+                float mag = desiredDirection.magnitude;
+                if (mag > 0)
+                {
+                    //transform.forward = desiredDirection;
+                    //Interpolar entre la rotación actual y la desesda
+
+                    Quaternion q = Quaternion.LookRotation(desiredDirection);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 10);
+                    transform.Translate(0, 0, speed * Time.deltaTime);
+                }
+
+                //Prueba del sistea, de vida
+                if (Input.GetKeyDown(KeyCode.T))
+                {
+                    TakeDamage(5);
+                }
             }
             
+            hud.labelHealth.text = health.Value.ToString();
         }
+    }
+
+    //RPC= 
+    [Rpc(SendTo.Server)]
+    public void TakeDamageRpc(int amount)
+    {
+        Debug.Log("RPC recibido TakeDamage");
+        TakeDamage(amount);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!IsAlive()) return;
+
+
+        if (!IsServer) 
+        {
+          TakeDamageRpc(amount);
+        }
+        else
+        {
+            health.Value -= amount;
+
+            if (health.Value <= 0)
+            {
+                health.Value = 0;
+
+                if (IsAlive())
+                {
+                    OnDeath();
+                }
+            }
+            else
+            {
+                audioSource.clip = DamageSound;
+                audioSource.Play();
+            }
+        }     
+    }
+
+    public void OnDeath()
+    {
+        //efectos
+        Debug.Log(name + " me muero");
+        audioSource.clip = DeathSound;
+        audioSource.Play();
+    }
+
+    public bool IsAlive()
+    {
+        return health.Value >= 0;
     }
 }
--- Assets/Proyecto/Scripts/UiManager.cs
+++ Assets/Proyecto/Scripts/UiManager.cs
@@ -7,12 +7,17 @@ public class UiManager : MonoBehaviour
 {
     public RectTransform PanelMainMenu;
     public RectTransform PanelClient;
+    public RectTransform PanelHUD;
+
+    public TMP_Text labelHealth;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         PanelMainMenu.gameObject.SetActive(true);
         PanelClient.gameObject.SetActive(false);
+        PanelHUD.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,6 +33,7 @@ public class UiManager : MonoBehaviour
         NetworkManager.Singleton.StartHost();
 
         PanelMainMenu.gameObject.SetActive(false);
+        PanelHUD.gameObject.SetActive(true);
     }
 
     public void OnButtonClientConnect()
@@ -35,7 +41,12 @@ public class UiManager : MonoBehaviour
         GameObject go = GameObject.Find("inputIP");
 
         string ip = go.GetComponent<TMP_InputField>().text;
-        
+        Debug.Log("Se conceto a " +  ip);
+
+        PanelMainMenu.gameObject.SetActive(false);
+        PanelClient.gameObject.SetActive(false);
+        PanelHUD.gameObject.SetActive(true);
+
         NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = ip;
         NetworkManager.Singleton.StartClient();
     } 
